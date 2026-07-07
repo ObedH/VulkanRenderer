acda84f8aa08ef5f86f7436e766386432c45096c
ObedH <obedhenryotto@gmail.com>
2026-07-07 18:04:46 -0400

    Added device property logging and device suitability checking

--- src/Device.cpp
+++ src/Device.cpp
@@ -29,6 +29,41 @@ void Device::log_device_properties(const vk::PhysicalDevice& device) {
 
 }
 
+bool Device::check_device_extension_support(const vk::PhysicalDevice& device, const std::vector<const char*>& requested_extension, bool debug) {
+
+
+
+}
+
+bool Device::is_suitable(const vk::PhysicalDevice& device, bool debug) {
+
+    const std::vector<const char*> requested_extensions = {
+        VK_KHR_SWAPCHAIN_EXTENSION_NAME
+    };
+
+    if(debug) {
+        std::cout << "[DEBUG]: Requesting the following device extensions:\n";
+        for(const char* extension : requested_extensions) {
+            std::cout << "\t" << extension << "\n";
+        }
+    }
+
+    if(bool extensions_supported = check_device_extension_support(device, requested_extensions, debug)) {
+        if(debug) {
+            std::cout << "[DEBUG]: Device can support the requested extensions\n";
+        }
+    }
+    else {
+        if(debug) {
+            std::cout << "[DEBUG]: Device cannot support the requested extensions\n";
+        }
+        return false;
+    }
+
+    return true;
+
+}
+
 vk::PhysicalDevice Device::choose_physical_device(vk::Instance& instance, bool debug) {
 
 
@@ -43,6 +78,9 @@ vk::PhysicalDevice Device::choose_physical_device(vk::Instance& instance, bool d
     for(vk::PhysicalDevice device : available_devices) {
         if(debug) {
             log_device_properties(device);
+        }
+        if(is_suitable(device, debug)) {
+            return device;
         }
     }
 
--- src/Device.hpp
+++ src/Device.hpp
@@ -4,7 +4,9 @@
 
 namespace Device {
     
+    bool check_device_extension_support(const vk::PhysicalDevice& device, const std::vector<const char*>& requested_extensions, bool debug);
     void log_device_properties(const vk::PhysicalDevice& device);
+    bool is_suitable(const vk::PhysicalDevice& device, bool debug);
     vk::PhysicalDevice choose_physical_device(vk::Instance& instance, bool debug);
 
 }
